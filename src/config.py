1c73176d82893af9121db82996f58f8799e00ace
gerrenme <gerrenme-s21@student.com>
2024-03-12 15:57:00 +0300

    add config

---
+++ src/config.py
@@ -0,0 +1,7 @@
+db_host: str = "127.0.0.1"
+db_user: str = "gerrenme"
+db_password: str = "1"
+db_name: str = "pythonMummy"
+
+admin_password: str = "1"
+telebot_key: str = "6861912111:AAG1HmkE2cRhhpgNAAnAl4aLJNiP_7nWlLA"
